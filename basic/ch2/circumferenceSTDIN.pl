7cfc452f8ee41d8c4a2c6443604c3eacc68515fe
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-01 17:32:23 +0800

    Update circumferenceSTDIN.pl

--- basic/ch2/circumferenceSTDIN.pl
+++ basic/ch2/circumferenceSTDIN.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 #count the circumference of the circle r = <STDIN>
+print"Counting the circumference of the circle\n";
+print"radius:";
 chomp($r = <STDIN>);
 $PI = 3.141592654;
 print"Input radius:";
 print"r = $r\n";
-print"r * 2 * PI = ".($r*2*$PI);
-
+print"circumference = r * 2 * PI = ".($r*2*$PI)."\n";
