33447a0aac9be0479a409fa859e67fa5fed0fb93
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-02 04:07:20 +0800

    change dir name

--- basic/ch3/inputStringList_reverse.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-print"Input a string list(separated by '\\n') and it will print the list reverse.\n";
-print"(Linux/Mac press Ctrl + D at the end, windows press Ctrl + Z)\n";
-print"Input:\n";
-chomp(@input = <STDIN>);
-@output = reverse(@input);
-print"\n";
-print"Output:\n";
-print"@output\n";
