990d33b3eaf5c53f0ca67ccbc5ff67e5a38f2b25
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-01 17:37:24 +0800

    Update circumferenceSTDINnegprocess.pl

--- basic/ch2/circumferenceSTDINnegprocess.pl
+++ basic/ch2/circumferenceSTDINnegprocess.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 #count the circumference of the circle r = <STDIN> and prevent <STDIN> < 0
+print"Counting the circumference of the circle\n";
+print"radius(not less than 0):";
 chomp($r = <STDIN>);
 $PI = 3.141592654;
 print"Input radius:";
@@ -7,5 +9,4 @@ print"r = $r\n";
 if($r < 0){
     $r = 0;
 }
-print"circumference:".($r*2*$PI);
-
+print"circumference:".($r*2*$PI)."\n";
