75f15c2a7ea2d06f8bb5489c999ee9ad6a6e30b7
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-02 02:54:13 +0800

    add more discription of input

--- basic/ch3/inputStringList_reverse.pl
+++ basic/ch3/inputStringList_reverse.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-print"Input some a string list and it will print it reverse.\n";
+print"Input some a string list(split with '\\n') and it will print it reverse.\n";
 print"(Linux/Mac press Ctrl + D at the end, windows press Ctrl + Z)\n";
 print"Input:\n";
 chomp(@input = <STDIN>);
