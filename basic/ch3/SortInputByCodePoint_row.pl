c681515633b85b9c85946b20faf2ea8541d32ad7
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-02 03:56:54 +0800

    Create SortInputByCodePoint_row.pl

---
+++ basic/ch3/SortInputByCodePoint_row.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+print"Input a string list(separated by '\\n') and it will print the list sorted by code point.\n";
+print"(Linux/Mac press Ctrl + D at the end, windows press Ctrl + Z)\n";
+print"Input:\n";
+chomp(@input = <STDIN>);
+@output = sort(@input);
+print"\n";
+print"Output:\n";
+foreach $string (@output){
+        print"$string\n";
+}
