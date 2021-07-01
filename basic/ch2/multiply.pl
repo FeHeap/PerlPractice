ba9e445395b66e8ad08d96423d4cebfb74c4db7d
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-01 17:43:19 +0800

    Update multiply.pl

--- basic/ch2/multiply.pl
+++ basic/ch2/multiply.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 #count the multiply of $a and $b
+print"count the multiply of a and b\n";
+print"a:";
 chomp($a = <STDIN>);
+print"b:";
 chomp($b = <STDIN>);
 print "multiply:".($a*$b)."\n";
