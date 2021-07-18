430dfa50073f1e641f046dbf05eac419f5a4025b
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-18 13:31:22 +0800

    Create greet.pl

---
+++ basic/ch4_Subroutine/greet.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use feature 'state';
+
+&greet( "Fred" );
+&greet( "Barney" );
+
+
+sub greet{
+        state @who;
+        if(@who == undef){
+                print "Hi @_! You are the first one here!\n";
+                @who = @_;
+        }
+        else{
+                print "Hi @_! @who is also here!\n";
+        }
+}
